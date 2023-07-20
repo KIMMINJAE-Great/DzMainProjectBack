cc9b51c80b4ad5ff66a14c80bec1cd3be5a0b421
KIM MINJAE <minjae2717@gmail.com>
2023-07-20 22:30:10 +0900

    [update] 메뉴권한 데이터 추출 기능 추가
    React 서버에서 사용할 메뉴를 사용자의 id를 기준으로 가져오는 기능을 추가하였습니다.
    
    기존의 emp_cd와 biz_nm의 의도가 유사하여 emp_cd하나로 통일 하였습니다.
    
    menu, mgroup, mauth 세개의 메뉴에 관한 테이블을 작성하였고 mauth를 통해 메뉴권한을 React서버로 보냈습니다.

---
+++ back/src/main/java/com/dz/back/dto/MauthDTO.java
@@ -0,0 +1,34 @@
+package com.dz.back.dto;
+
+public class MauthDTO {
+	private String menucd;
+    private String mgroupcd;
+    private String url;
+    private String menunm;
+	public String getMenucd() {
+		return menucd;
+	}
+	public void setMenucd(String menucd) {
+		this.menucd = menucd;
+	}
+	public String getMgroupcd() {
+		return mgroupcd;
+	}
+	public void setMgroupcd(String mgroupcd) {
+		this.mgroupcd = mgroupcd;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getMenunm() {
+		return menunm;
+	}
+	public void setMenunm(String menunm) {
+		this.menunm = menunm;
+	}
+    
+
+}
